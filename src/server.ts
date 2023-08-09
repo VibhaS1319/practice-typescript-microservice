ffdfaad4cce6d3bd10d1642b54110ade127a2b1c
VibhaS1319 <shikharagarwal19@gmail.com>
2023-08-09 16:53:10 +0530

    changes in response

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,6 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.get("/", (req, res) => res.send("Hello from server!"));
+app.get("/", (req, res) => res.send("Hello, Congratualtions for completing set up!"));
 
 app.listen(PORT, () => console.log(`⚡Server is running here 👉 https://localhost:${PORT}`));
