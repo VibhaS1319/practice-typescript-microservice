4a0a7154749461303c6cd6fc6f393c6d0f5a6b1b
Vibha singh <124451007+VibhaS1319@users.noreply.github.com>
2023-08-12 21:44:16 +0530

    Update BaseController.ts

--- src/controller/BaseController.ts
+++ src/controller/BaseController.ts
@@ -3,12 +3,6 @@ import baseService from "../service/baseService";
 
 export default class BaseController{
    
-    //  model:any;
-    // constructor(model:any){
-    //     model = model;
-    //     console.log("model: "+model);
-    // }
-
      getAllData= async (req: Request, res: Response<any>, model:any) =>{
         const allData = await new baseService().getAllData(model);
         return allData;
